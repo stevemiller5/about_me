1207ace9a22f680cc2c72160094baae67a4c7978
Steve Miller <ssgz.miller@gmail.com>
2018-08-18 21:55:18 -0700

    scaffold html add css styling to body add js alert

---
+++ app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// var name = prompt('What is your name');
+// console.log(name);
+
+// var homeTown = prompt('Hi ' +name + ' Where are you from?');
+
+// let growingUp = prompt(`So ${name} how was it growing up in ${homeTown}`);
+
